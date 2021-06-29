db8cd68227a8247667a6b1c0226f2520aa19cc99
bnt0106 <1952222@gm.uit.edu.vn>
2021-06-29 20:48:45 +0700

    sua account.php

--- page/Main/Account.php
+++ page/Main/Account.php
@@ -17,10 +17,7 @@
 
   
 ?>
-<?php
-          
-if(empty($_SESSION['customer'])){
-  ?>
+
             
 
 <div class="container">
@@ -33,7 +30,7 @@ if(empty($_SESSION['customer'])){
               <a class="bca" >By creating an account with our store, you will be able to move through the checkout process faster, store multiple shipping addresses, view and track your orders in your account and more.</a>
           </p>
           <div class="register_wrap">
-            <a href = "/SaleOnlineWebDeveloper/page/index.php?manager=Register" class = "login1" > CREATE ACCOUNT </a>
+            <a href = "/SaleOnlineWebDeveloper/page/index.php?manager=Register" class = "creat_account" > CREATE ACCOUNT </a>
           </div>    
         </div>
           <div id="login">
@@ -45,7 +42,7 @@ if(empty($_SESSION['customer'])){
               <h2 class="customers">REGISTERED CUSTOMERS</h2>
               <input type="text" class="form-control" placeholder="user name" id ="username" name="username" required />
               <br>
-              <input type="password" class="form-control" id ="password" name="password" placeholder="password" />
+              <input type="password" class="form-control" id ="password" name="password" placeholder="password" required />
               <br>
               <a href="#" class="lk"> forgot password?</a>
               <br>
@@ -54,46 +51,8 @@ if(empty($_SESSION['customer'])){
                 
           </div>
           
+ </div>
           
-        
-
-          <?php
-          }else{
-
-
-          
-          
-            ?>
-            <div class="container">
-        
-        <h1 class="account_td">ACCOUNT</h1>
-  
-                
-          <div id="register">
-            <form id ="register_form"action="/SaleOnlineWebDeveloper/page/register.php" method="POST">
-            
-              <h2 class="register_td">REGISTER</h2>
-              <input type="text" class="flname"  id ="firstname" name="firstname" placeholder="first name" required />
-              <input type="text" class="flname"  id ="lastname" name="lastname" placeholder="last name" required />
-              <br>
-              
-              <input type="text" placeholder="user name" id ="username" name="username" required />
-              <br>
-              <input type="password"  id ="password" name="password" placeholder="password"required />
-              
-              <br>
-              <input class="login1"   type="submit" id="btn" name="btn-save" value="CREATE">
-              <br>
-              <a href="/SaleOnlineWebDeveloper/page/index.php?manager=Account&id=1" class="bca"> Back to login</a>
-            </form>
-             
-              
-                
-          </div>
-           
-           <?php
-            }
-          ?>
           <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
           <script> 
         
@@ -132,4 +91,4 @@ if(empty($_SESSION['customer'])){
 
 
 
-          </div>
+          
